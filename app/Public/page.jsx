2201d299441ae524799c08477a0db5124ff1c157
markusgemeinder <190774@gmx.de>
2024-07-20 16:41:38 +0200

    Implement /public/screenshots and /public/videos (testvideos)

--- app/Public/page.jsx
+++ app/Public/page.jsx
@@ -4,6 +4,12 @@ const Public = () => {
   return (
     <div>
       <h1>Public</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <video width='80%' controls>
+          <source src='/videos/Testvideo_16-9_LOW.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 };
