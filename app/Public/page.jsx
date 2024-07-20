7cba320d1c672b4f99d89278d9f91bc39399e517
markusgemeinder <190774@gmx.de>
2024-07-20 16:51:45 +0200

    Install testvideo to app/Public/page.jsx

--- app/Public/page.jsx
+++ app/Public/page.jsx
@@ -5,8 +5,12 @@ const Public = () => {
     <div>
       <h1>Public</h1>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <video width='80%' controls>
-          <source src='/videos/Testvideo_16-9_LOW.mp4' type='video/mp4' />
+        <video width='20%' controls>
+          <source src='/videos/testvideo_16-9_high.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+        <video width='20%' controls>
+          <source src='/videos/testvideo_4-5_high.mp4' type='video/mp4' />
           Your browser does not support the video tag.
         </video>
       </div>
